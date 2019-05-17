4f4d7fd54910c9e5ecda80b86f060d0a14cfe5d7
Roshan Doddanavar <roshandoddanavar@gmail.com>
2019-05-16 21:27:58 -0500

    `Input` class - working instance

--- src/input.cpp
+++ src/input.cpp
@@ -8,14 +8,30 @@ namespace py = pybind11;
 // Class definition 
 class Input 
 {
-    double value;
-    double min;
-    double max;
+    public:
+
+        // Function(s)
+        Input(); // Constructor 
+
+        // Data
+        double value = 0.0;
+        double min   = 0.0;
+        double max   = 0.0;
 };
+
+// Member function definition(s)
+Input::Input(void)
+{
+    // Empty constructor 
+}
 
 // Binding code
 PYBIND11_MODULE(input, m)
 {
     m.doc() = "sim input module"; // optional module docstring
-    py::class_<Input>(m, "Input");
+    py::class_<Input>(m, "Input")
+        .def(py::init<>())
+        .def_readwrite("value", &Input::value)
+        .def_readwrite("min", &Input::min)
+        .def_readwrite("max", &Input::max);
 }
