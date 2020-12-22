81089a5b19abe89ebdb4f0b8d22fdbd3ffa0aac9
rdoddanavar <roshandoddanavar@gmail.com>
2020-12-22 14:08:11 -0600

    Added proper *.eng & *.rse files for test motor; RSE file out of scope for now

---
+++ src/preproc/preproc_model.py
@@ -0,0 +1,21 @@
+'''
+High Power Rocketry - Flight Simulation
+MIT License
+Copyright (C) 2019 Roshan Doddanavar
+https://rdoddanavar.github.io
+
+Path:
+    hpr-sim/src/preproc/preproc_model.py
+Created:
+    2020-12-22
+Type:
+    Python3 module
+Description:
+    < >
+Functions:
+    < >
+Classes:
+    < >
+Dependencies:
+    < >
+'''
