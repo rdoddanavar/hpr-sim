d2bbefe9902cc9f3fc72e7f5b5a66f65abf2372a
rdoddanavar <roshandoddanavar@gmail.com>
2020-12-22 23:44:13 -0600

    Standalone prop preprocessing

---
+++ src/preproc/preproc_prop.py
@@ -0,0 +1,40 @@
+'''
+High Power Rocketry - Flight Simulation
+MIT License
+Copyright (C) 2019 Roshan Doddanavar
+https://rdoddanavar.github.io
+
+Path:
+    hpr-sim/src/preproc/preproc_prop.py
+Created:
+    2020-12-22
+Type:
+    Python3 module
+Description:
+    < >
+Functions:
+    < >
+Classes:
+    < >
+Dependencies:
+    < >
+'''
+
+# System modules
+import sys
+import pdb
+from pathlib import Path
+
+def load(propPath):
+    pass
+
+def load_eng(propPath):
+    pass
+
+def load_rse(propPath):
+    pass
+
+if __name__ == "__main__":
+
+    # CLI argument
+    pass
