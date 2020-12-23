8470e94977b0794db1319b9f82bd5af616c324d2
rdoddanavar <roshandoddanavar@gmail.com>
2020-12-23 01:19:52 -0600

    Initial ENG file processing

--- src/preproc/preproc_prop.py
+++ src/preproc/preproc_prop.py
@@ -24,17 +24,57 @@ Dependencies:
 import sys
 import pdb
 from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
 
 def load(propPath):
-    pass
+    
+    ext = Path(propPath).suffix
+
+    if ext == ".eng":
+        load_eng(propPath)
+    elif ext == ".rse":
+        load_rse(propPath)
+    else:
+        raise ValueError('Unknown engine file extension', ext)
 
 def load_eng(propPath):
-    pass
+    
+    # Initialize assumed (0,0) point
+    time   = np.array([0])
+    thrust = np.array([0])
+
+    with open(propPath, 'r') as engFile:
+        for line in engFile.read().splitlines():
+            
+            if ';' in line:
+                continue # Skip header comments
+            
+            words = line.strip().split(' ')
+
+            if len(words) > 2:
+                
+                # Process info line:
+                # motor name, diameter [mm], length [mm], delays, prop. weight [kg], tot. weight [kg], manufacturer
+                diameter    = float(words[1]) 
+                length      = float(words[2])
+                propWeight  = float(words[4])
+                totalWeight = float(words[5])
+
+            elif len(words) == 2: 
+                
+                # Process thrust data lines:
+                # time [sec], thrust [N]
+                time   = np.append(time,   float(words[0]))
+                thrust = np.append(thrust, float(words[1]))
+
+    plt.plot(time, thrust)
+    plt.show()
 
 def load_rse(propPath):
     pass
 
 if __name__ == "__main__":
 
-    # CLI argument
-    pass
+    propPath = sys.argv[1]
+    load(propPath)
