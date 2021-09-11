71e82c5b70df2b6ce624e77e95495622fee7ae15
rdoddanavar <roshandoddanavar@gmail.com>
2021-09-11 16:19:46 -0700

    rng work

--- src/exec/exec.py
+++ src/exec/exec.py
@@ -76,18 +76,23 @@ def exec(inputPath, outputPath):
 
     flight.init(t0, dt, tf)
 
-    # Sim execution
-    iRun = list(range(nProc))
+    # # Sim execution
+    #iRun = list(range(nProc))
     pool = mp.Pool(nProc)
 
-    #pool.map_async(functools.partial(run_mc, flight), iRun)
+    # pool.map_async(functools.partial(run_mc, flight), iRun)
     flights = [flight]*nProc
-    breakpoint()
+    # ##breakpoint()
     pool.map_async(test, flights)
+
+    #pool.map_async(run_mc2, iRun)
 
     pool.close()
     pool.join()
 
+    # #flight.update()
+    # #flight.write_telem("output/output.csv")
+    
     # Post-processing
 
 #------------------------------------------------------------------------------#
@@ -104,8 +109,78 @@ def run_mc(flight, iRun):
     outputPath = f"output/output{iRun}.csv"
     flight.write_telem(outputPath)
 
-def test(flight):
+def run_mc2(iRun):
+
+    print("test1")
+    inputPath = "input/unit_test.yml"
+
+    # Pre-processing
+    util_unit.config()
+
+    configPath = pathlib.Path(__file__).parent / configPathRel
+    configPath = str(configPath.resolve())
+    configDict = util_yaml.load(configPath)
+
+    inputDict = util_yaml.load(inputPath)
+    util_yaml.process(inputDict)
+    preproc_input.process(inputDict, configDict)
+
+    print("test2")
+
+    # Initialize model - engine
+    enginePath = inputDict["engine"]["inputPath"]["value"]
+    timeEng, thrustEng, massEng = preproc_engine.load(enginePath)
+
+    engine = model.Engine()
+    engine.init(timeEng, thrustEng, massEng)
+
+    # Initialize model - mass
+    mass     = model.Mass()
+    massBody = inputDict["mass"]["massBody"]["value"]
+
+    #--------------------#
+    massBody += iRun
+    #--------------------#
+
+    mass.init(massBody)
+    mass.add_dep(engine)
+
+    # Initialize model - geodetic
+    geodetic = model.Geodetic()
+    latitude = inputDict["geodetic"]["latitude"]["value"]
+
+    geodetic.init(latitude)
+
+    # Initialize model - EOM
+    eom = model.EOM()
+    eom.init()
+
+    eom.add_dep(engine)
+    eom.add_dep(mass)
+    eom.add_dep(geodetic)
+
+    # Initialize model - flight
+    flight = model.Flight()
+    flight.add_dep(eom)
+
+    t0 = 0.0
+    dt = 0.01
+    tf = 50.0
+
+    flight.init(t0, dt, tf)
+
     flight.update()
+    #outputPath += f"{outputName}{iRun}.csv"
+    outputPath = f"output/output{iRun}.csv"
+    flight.write_telem(outputPath)
+
+def test(flight):
+    print("done!")
+    flight.update()
+    print("done!")
+
+def test2(num):
+    print(str(num))
 
 #------------------------------------------------------------------------------#
 
