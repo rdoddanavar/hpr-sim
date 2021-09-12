666b314a53296d8c89344a8f9bba34824b5be055
rdoddanavar <roshandoddanavar@gmail.com>
2021-09-11 17:42:28 -0700

    Exec params setup, par pool cleaned up

--- src/exec/exec.py
+++ src/exec/exec.py
@@ -21,11 +21,11 @@ import model
 
 # Module variables
 configPathRel = "../../config/config_input.yml"
-nProc = 4
+outputPath    = None
 
 #------------------------------------------------------------------------------#
 
-def exec(inputPath, outputPath):
+def exec(inputPath, outputPathBase):
 
     # Pre-processing
     util_unit.config()
@@ -38,94 +38,28 @@ def exec(inputPath, outputPath):
     util_yaml.process(inputDict)
     preproc_input.process(inputDict, configDict)
 
-    # Initialize model - engine
-    enginePath = inputDict["engine"]["inputPath"]["value"]
-    timeEng, thrustEng, massEng = preproc_engine.load(enginePath)
-
-    engine = model.Engine()
-    engine.init(timeEng, thrustEng, massEng)
-
-    # Initialize model - mass
-    mass     = model.Mass()
-    massBody = inputDict["mass"]["massBody"]["value"]
-
-    mass.init(massBody)
-    mass.add_dep(engine)
-
-    # Initialize model - geodetic
-    geodetic = model.Geodetic()
-    latitude = inputDict["geodetic"]["latitude"]["value"]
-
-    geodetic.init(latitude)
-
-    # Initialize model - EOM
-    eom = model.EOM()
-    eom.init()
-
-    eom.add_dep(engine)
-    eom.add_dep(mass)
-    eom.add_dep(geodetic)
-
-    # Initialize model - flight
-    flight = model.Flight()
-    flight.add_dep(eom)
-
-    t0 = 0.0
-    dt = 0.01
-    tf = 50.0
-
-    flight.init(t0, dt, tf)
+    breakpoint()
 
     # # Sim execution
-    #iRun = list(range(nProc))
-    pool = mp.Pool(nProc)
+    numProc = inputDict['exec']['numProc']['value']
+    numMC   = inputDict['exec']['numMC']['value']
+    numMC   = inputDict['exec']['seedMaster']['value']
 
-    # pool.map_async(functools.partial(run_mc, flight), iRun)
-    flights = [flight]*nProc
-    # ##breakpoint()
-    pool.map_async(test, flights)
+    pool  = mp.Pool(numProc)
+    intMC = range(numMC)
+    seeds = np.array(intMC)
+    seeds += seedMaster
 
-    #pool.map_async(run_mc2, iRun)
+    pool.map_async(run_mc, seeds)
 
     pool.close()
     pool.join()
-
-    # #flight.update()
-    # #flight.write_telem("output/output.csv")
     
     # Post-processing
 
 #------------------------------------------------------------------------------#
 
-outputPath = "output/"
-outputName = "output"
-
-def run_mc(flight, iRun):
-
-    print("test")
-
-    flight.update()
-    #outputPath += f"{outputName}{iRun}.csv"
-    outputPath = f"output/output{iRun}.csv"
-    flight.write_telem(outputPath)
-
-def run_mc2(iRun):
-
-    print("test1")
-    inputPath = "input/unit_test.yml"
-
-    # Pre-processing
-    util_unit.config()
-
-    configPath = pathlib.Path(__file__).parent / configPathRel
-    configPath = str(configPath.resolve())
-    configDict = util_yaml.load(configPath)
-
-    inputDict = util_yaml.load(inputPath)
-    util_yaml.process(inputDict)
-    preproc_input.process(inputDict, configDict)
-
-    print("test2")
+def run_mc(seed):
 
     # Initialize model - engine
     enginePath = inputDict["engine"]["inputPath"]["value"]
@@ -173,14 +107,6 @@ def run_mc2(iRun):
     #outputPath += f"{outputName}{iRun}.csv"
     outputPath = f"output/output{iRun}.csv"
     flight.write_telem(outputPath)
-
-def test(flight):
-    print("done!")
-    flight.update()
-    print("done!")
-
-def test2(num):
-    print(str(num))
 
 #------------------------------------------------------------------------------#
 
