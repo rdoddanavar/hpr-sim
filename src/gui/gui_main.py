ac5df5ece7cce3485528784da7f7a67aa575c39f
rdoddanavar <roshandoddanavar@gmail.com>
2024-08-02 23:45:41 -0700

    Starting GUI

---
+++ src/gui/gui_main.py
@@ -0,0 +1,19 @@
+from PySide6.QtWidgets import QApplication, QWidget
+
+# Only needed for access to command line arguments
+import sys
+
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([]) works too.
+gui = QApplication(sys.argv)
+
+# Create a Qt widget, which will be our window.
+window = QWidget()
+window.show()  # IMPORTANT!!!!! Windows are hidden by default.
+
+# Start the event loop.
+gui.exec()
+
+# Your application won't reach here until you exit and the event
+# loop has stopped.
