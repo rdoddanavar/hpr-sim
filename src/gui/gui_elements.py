db5c5451d6cfb6691fe40fefc75dfc6e081e5526
rdoddanavar <roshandoddanavar@gmail.com>
2024-08-12 23:35:56 -0700

    Simulation tab WIP

--- src/gui/gui_elements.py
+++ src/gui/gui_elements.py
@@ -5,7 +5,16 @@ from PyQt5.QtWidgets import(
     QPushButton,
     QTabWidget,
     QWidget,
+    QGridLayout,
+    QGroupBox,
+    QLabel,
+    QScrollArea,
+    QTextEdit,
+    QSizePolicy,
+    QProgressBar
 )
+
+from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
 
@@ -23,8 +32,8 @@ class MainWindow(QMainWindow):
         #tabOptimization = TabOptimization()
 
         tabs = QTabWidget()
-        tabs.insertTab(0, tabInput , "Input" )
-        tabs.insertTab(1, tabOutput, "Output")
+        tabs.insertTab(0, tabInput , "Simulation" )
+        tabs.insertTab(1, tabOutput, "Data")
 
         self.setCentralWidget(tabs)
 
@@ -33,6 +42,62 @@ class TabInput(QWidget):
     def __init__(self):
 
         super().__init__()
+
+        #---------------------------------------------------------------------#
+
+        groupIO = QGroupBox("Simulation I/O")
+
+        layoutIO = QGridLayout()
+        buttonInput = QPushButton("get input")
+        buttonInput.setSizePolicy(QSizePolicy.Preferred,QSizePolicy.Preferred)
+        textInput = QTextEdit("file/path")
+
+        layoutIO.addWidget(buttonInput, 0, 0)
+        layoutIO.addWidget(textInput, 0, 1)
+        groupIO.setLayout(layoutIO)
+
+        #---------------------------------------------------------------------#
+
+        groupParam = QGroupBox("Simulation Parameters")
+
+        label  = QLabel()
+        scroll = QScrollArea()
+
+        for i in range(10):
+            label.setText(label.text() + f"test line {i}\n")
+
+        scroll.setWidget(label)
+        scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        scroll.setWidgetResizable(True)
+
+        layoutParam = QGridLayout()
+        layoutParam.addWidget(scroll, 0, 0)
+        groupParam.setLayout(layoutParam)
+
+        #---------------------------------------------------------------------#
+
+        groupControl = QGroupBox("Simulation Control")
+
+        buttonRun = QPushButton("Run Sim")
+        buttonRun.setSizePolicy(QSizePolicy.Preferred,QSizePolicy.Preferred)
+
+        progressBar = QProgressBar()
+        progressBar.setValue(50)
+
+        layoutControl = QGridLayout()
+        layoutControl.addWidget(buttonRun, 0, 0)
+        layoutControl.addWidget(progressBar, 0, 1)
+        groupControl.setLayout(layoutControl)
+
+        #---------------------------------------------------------------------#
+
+        layoutTab = QGridLayout()
+        layoutTab.addWidget(groupIO, 0, 0, 1, 1)
+        layoutTab.addWidget(groupParam, 1, 0, 1, 1)
+        layoutTab.addWidget(groupControl, 2, 0, 1, 1)
+
+        self.setLayout(layoutTab)
 
 class TabOutput(QWidget):
 
