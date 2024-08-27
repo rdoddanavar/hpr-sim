7d9fa1bb773050d9696d66403e6ed72705346c34
rdoddanavar <roshandoddanavar@gmail.com>
2024-08-26 23:09:48 -0700

    action

--- src/gui/gui_actions.py
+++ src/gui/gui_actions.py
@@ -7,6 +7,9 @@ from PyQt5.QtWidgets import(
 
 # Project modules
 import exec
+import postproc_flight
+
+#------------------------------------------------------------------------------#
 
 def get_file_action(parent, target):
     return functools.partial(get_file, parent, target)
@@ -16,6 +19,8 @@ def get_file(parent, target):
     fileName = QFileDialog.getOpenFileName(parent, "Select Input File", pathlib.Path(".").resolve().as_posix(), "Input Files (*.yml *.yaml)")
     target.setText(fileName[0])
 
+#------------------------------------------------------------------------------#
+
 def get_directory_action(parent, target):
     return functools.partial(get_directory, parent, target)
 
@@ -24,15 +29,31 @@ def get_directory(parent, target):
     directory = QFileDialog.getExistingDirectory(parent, "Select Output Directory", pathlib.Path(".").resolve().as_posix())
     target.setText(directory)
 
+#------------------------------------------------------------------------------#
+
 def run_exec_action(lineInput, lineOutput):
     return functools.partial(run_exec,lineInput,lineOutput)
 
 def run_exec(lineInput, lineOutput):
-    
+
     inputPath  = pathlib.Path(lineInput.text())
     outputPath = pathlib.Path(lineOutput.text())
     configPath = pathlib.Path(__file__).parent.parent.parent / "config"
     exec.run(inputPath, outputPath, configPath)
+
+#------------------------------------------------------------------------------#
+
+def load_data_action(parent, lineOutput, comboField, comboUnits, spinRunNum):
+    return functools.partial(load_data, parent, lineOutput, comboField, comboUnits, spinRunNum)
+
+def load_data(parent, lineOutput, comboField, comboUnits, spinRunNum):
+
+    outputPath   = pathlib.Path(lineOutput.text())
+    parent.telem = postproc_flight.load_mc(outputPath)
+
+    #comboField
+
+#------------------------------------------------------------------------------#
 
 if __name__ == "__main__":
     pass
