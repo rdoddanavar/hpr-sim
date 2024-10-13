d5738579c41ff5d31e2ff52801b120d9403d7768
rdoddanavar <roshandoddanavar@gmail.com>
2024-10-12 20:51:50 -0700

    Regression test script working well

--- tools/regression_test.py
+++ tools/regression_test.py
@@ -1,22 +1,53 @@
 import sys
+import os
 import difflib
 import filecmp
+import pathlib
+import shutil
 
-def regression():
+def regression_test(dir1: pathlib.Path, dir2: pathlib.Path) -> None:
 
-    file1 = open("output/unit_test/run0/stats.yml",'r')
-    file2 = open("output/unit_test/run1/stats.yml",'r')
+    dcmp = filecmp.dircmp(dir1, dir2)
 
-    lines1 = file1.read().splitlines()
-    lines2 = file2.read().splitlines()
+    outputPath = pathlib.Path("output/diff")
+
+    if os.path.exists(outputPath):
+        shutil.rmtree(outputPath)
+
+    os.mkdir(outputPath)
+
+    # Compare files
+
+    for file in dcmp.diff_files:
+        diff_files(dir1 / file, dir2 / file, outputPath / file)
+
+    # Compare subdirectories; assume one (1) nested level
+
+    for subdir in dcmp.subdirs:
+
+        os.mkdir(outputPath / subdir)
+
+        for file in dcmp.subdirs[subdir].diff_files:
+            diff_files(dir1 / subdir / file, dir2 / subdir / file, outputPath / subdir / file)
+
+def diff_files(filePath1, filePath2, outputPath):
+
+    file1 = open(filePath1, 'r')
+    file2 = open(filePath2, 'r')
+
+    # Burn first line to skip time stamp (always expected to differ)
+    lines1 = file1.read().splitlines()[1:]
+    lines2 = file2.read().splitlines()[1:]
 
     diff = difflib.HtmlDiff().make_file(lines1, lines2, "Before", "After")
 
-    fileDiff = open("diff.html", 'w')
+    fileDiff = open(outputPath.as_posix() + ".html", 'w')
     fileDiff.write(diff)
     fileDiff.close()
 
-    # filecmp.cmp('output/unit_test/run0/stats.yml','output/unit_test/run1/stats.yml')
-
 if __name__ == "__main__":
-    regression()
+
+    dir1 = pathlib.Path("tools/unit_test")
+    dir2 = pathlib.Path("output/unit_test")
+
+    regression_test(dir1, dir2)
