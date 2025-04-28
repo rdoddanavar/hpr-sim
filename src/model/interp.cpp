dffa73c5056a2cd1883c57dd327c89a0f38bf83c
rdoddanavar <roshandoddanavar@gmail.com>
2025-04-27 19:45:20 -0700

    Cleanup

--- src/model/interp.cpp
+++ src/model/interp.cpp
@@ -33,9 +33,9 @@ std::vector<double> process_numpy_array(const numpyArray& array)
 void Interp::init(std::vector<std::vector<double>> xData, std::vector<double> yData, interpMethod method)
 {
 
-    xData_ = xData;
-    yData_ = yData;
-    method_  = method;
+    xData_  = xData;
+    yData_  = yData;
+    method_ = method;
 
     nDim_ = xData_.size();
 
@@ -78,6 +78,8 @@ void Interp::init(std::vector<std::vector<double>> xData, std::vector<double> yD
 
 }
 
+//---------------------------------------------------------------------------//
+
 void Interp::check_method(std::string methodName, size_t methodDim)
 {
 
@@ -99,10 +101,14 @@ void Interp::check_method(std::string methodName, size_t methodDim)
 
 }
 
+//---------------------------------------------------------------------------//
+
 void Interp::init_linear()
 {
     check_method("LINEAR", 1);
 }
+
+//---------------------------------------------------------------------------//
 
 double Interp::update(double xq)
 {
@@ -119,17 +125,25 @@ double Interp::update(double xq)
     return yq;
 }
 
+//---------------------------------------------------------------------------//
+
 double Interp::update(std::vector<double> xq)
 {
+
     double yq;
+
     switch (method_)
     {
         case BILINEAR:
             yq = update_bilinear(xq);
             break;
     }
+
     return yq;
+
 }
+
+//---------------------------------------------------------------------------//
 
 double Interp::update_linear(double xq)
 {
@@ -195,21 +209,20 @@ void Interp::init_pchip()
     // 2. Compute derviative values for PCHIP method
     pchip_.d.reserve(nX);
 
-    // 2A. Derivative at lower endpoint, use three-point method:
-    //     Correct value to perserve shape if necessary
+    // 2A. Derivative at lower endpoint, use three-point method;
+    //     correct value to perserve shape if necessary
     pchip_.d[0] = deriv_three_point({x[0], x[1], x[2]}, {y[0], y[1], y[2]}, x[0]);
 
     if (sgn(pchip_.d[0]) != sgn(pchip_.del[0])) {pchip_.d[0] = 0.0;}
 
-    // 2B. Derivative at upper endpoint, use three-point method:
-    //     Correct value to perserve shape if necessary
+    // 2B. Derivative at upper endpoint, use three-point method;
+    //     correct value to perserve shape if necessary
     pchip_.d[nX-1] = deriv_three_point({x[nX-3], x[nX-2], x[nX-1]}, {y[nX-3], y[nX-2], y[nX-1]}, x[nX-1]);
 
     if (sgn(pchip_.d[nX-1]) != sgn(pchip_.del[nX-2])) {pchip_.d[nX-1] = 0.0;}
 
     // 2C. Derivatives at all other points:
-    //     If signs of adjacent linear slopes (delta) don't match 
-    //     or are zero, set derivative to zero;
+    //     If signs of adjacent linear slopes (delta) don't match or are zero, set derivative to zero;
     //     otherwise, if signs match compute derivative as weighted average
     double w1, w2;
 
@@ -217,13 +230,15 @@ void Interp::init_pchip()
     {
         if (sgn(pchip_.del[iX])*sgn(pchip_.del[iX-1]) > 0)
         {
-            w1    = 2*pchip_.h[iX] + pchip_.h[iX-1];
-            w2    = pchip_.h[iX] + 2*pchip_.h[iX-1];
+            w1 = 2*pchip_.h[iX] + pchip_.h[iX-1];
+            w2 = pchip_.h[iX] + 2*pchip_.h[iX-1];
             pchip_.d[iX] = (w1 + w2)/(w1/pchip_.del[iX-1] + w2/pchip_.del[iX]);
         }
     }
 
 }
+
+//---------------------------------------------------------------------------//
 
 double Interp::deriv_three_point(std::vector<double> x, std::vector<double> y, double xi)
 {
@@ -256,10 +271,10 @@ double Interp::update_pchip(double xq)
 
         // Get coefficients
         const size_t iX = iS_[iDim];
-        double s = xq - xData_[iDim][iX];
+        const double s  = xq - xData_[iDim][iX];
 
-        double ck = (3*pchip_.del[iX] - 2*pchip_.d[iX] - pchip_.d[iX+1])/pchip_.h[iX];
-        double bk = (pchip_.d[iX] - 2*pchip_.del[iX] + pchip_.d[iX+1])/pow(pchip_.h[iX], 2);
+        const double ck = (3*pchip_.del[iX] - 2*pchip_.d[iX] - pchip_.d[iX+1])/pchip_.h[iX];
+        const double bk = (pchip_.d[iX] - 2*pchip_.del[iX] + pchip_.d[iX+1])/pow(pchip_.h[iX], 2);
 
         // Power form interpolation
         yq = yData_[iX] + s*pchip_.d[iX] + pow(s, 2)*ck + pow(s, 3)*bk;
@@ -381,7 +396,9 @@ size_t Interp::search(const std::vector<double>& x, double xq, size_t iS)
         // goal: find index of (floor) closest x point)
 
         if ((xq >= x[iS]) && (xq < x[iS+1]))
+        {
             return iS;
+        }
         else
         {
             if (xq < x[iS])
